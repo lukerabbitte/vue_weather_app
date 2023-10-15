2b6d96fd6cf76f10d4c5c31ca35b7fb1a40aa1c2
lukerabbitte <luke.rabbitte@gmail.com>
2023-10-15 18:15:53 +0100

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const currentLanguage = ref('en');
+
+const toggleLanguage = () => {
+  currentLanguage.value = currentLanguage.value === 'en' ? 'fr' : 'en';
+};
+
+</script>
+
+<template>
+    <nav class="navbar">
+      <button @click="toggleLanguage">{{ currentLanguage }}</button>
+    </nav>
+</template>
